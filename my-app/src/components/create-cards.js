1dea72d62548cc387e5ac504ba429092849d0559
caoamyc <caoamyc@sfu.ca>
2023-03-04 15:14:26 -0800

    add make request for api call

--- my-app/src/components/create-cards.js
+++ my-app/src/components/create-cards.js
@@ -3,46 +3,39 @@ import { useState } from "react";
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: 'UR-API-KEY-HERE',
 });
 const openai = new OpenAIApi(configuration);
 
 
 async function MakeRequest(prompt){
   if (!configuration.apiKey) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
+    alert("OpenAI API key not configured, please follow instructions in README.md")
     return;
   }
   try {
-    const completion = await createCompletion({
+    const completion = await openai.createCompletion({
       model: "gpt-3.5-turbo",
-      prompt: "create simple questions and answers separated by a new line using the text 'Apples are fruits that are red.' with at most 1 pairs of questions and answers",
-      temperature: 0.6,
-      apiKey: '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'
+      messages: [
+        {"role": "system", "content": "You are a helpful assistant that generates questions and answers from a text"},
+        {"role": "user", "content": "create simple questions and answers separated by a new line using the text 'Apples are fruits that are red.' with at most 1 pairs of questions and answers"},
+      ],
+        temperature: 0.6,
     });
-    console.log("response:", completion)
+    console.log("response:", completion.data.choices[0].text)
   
-    res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
     } else {
       console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
+      
+      };
     }
   }
   // console.log(prompt)
-}
+
 
 
 function CreateCards() {
