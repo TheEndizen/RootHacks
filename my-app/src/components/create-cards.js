5046838355fb94298e4c82f2c4b3d7afbb5f51a4
Denise Siu <68887691+djsiu@users.noreply.github.com>
2023-03-04 17:13:21 -0800

    connect openai api

--- my-app/src/components/create-cards.js
+++ my-app/src/components/create-cards.js
@@ -2,62 +2,60 @@ import { useState } from "react";
 
 import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  apiKey: 'UR-API-KEY-HERE',
-});
-const openai = new OpenAIApi(configuration);
+const CreateCards = () => {
+	let jsonOutput;
+	const chatgptrequest = async (e) => {
+		try {
+			await fetch(`https://api.openai.com/v1/completions`, {
+				body: JSON.stringify({
+					model: "text-davinci-003",
+					prompt: userInput,
+					temperature: 0.3,
+					max_tokens: 2000,
+				}),
+				method: "POST",
+				headers: {
+					"content-type": "application/json",
+					Authorization:
+						"Bearer " + "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",
+				},
+			}).then((response) => {
+				console.log(response); //If you want to check the full response
+				if (response.ok) {
+					response.json().then((json) => {
+						console.log(json); //If you want to check the response as JSON
+						console.log(json.choices[0].message.content); //HERE'S THE CHATBOT'S RESPONSE
+						jsonOutput = json.choices[0].message.content;
+					});
+				}
+			});
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
 
+	const [userInput, setUserInput] = useState("");
 
-async function MakeRequest(prompt){
-  if (!configuration.apiKey) {
-    alert("OpenAI API key not configured, please follow instructions in README.md")
-    return;
-  }
-  try {
-    const completion = await openai.createCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {"role": "system", "content": "You are a helpful assistant that generates questions and answers from a text"},
-        {"role": "user", "content": "create simple questions and answers separated by a new line using the text 'Apples are fruits that are red.' with at most 1 pairs of questions and answers"},
-      ],
-        temperature: 0.6,
-    });
-    console.log("response:", completion.data.choices[0].text)
-  
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      
-      };
-    }
-  }
-  // console.log(prompt)
+	const handleChange = (e) => {
+		setUserInput(e.target.value);
+	};
 
-
-
-function CreateCards() {
-  const [prompt, setPrompt] = useState()
-  const chatgptrequest = () => {
-    // make chatgpt request
-    MakeRequest(prompt)
-  }
-  const change = event => {
-    // alert(event)
-    setPrompt (event.target.value)
-  }
-  return (
-    <div className = "Component">
-      <h2>Make Flash Cards!</h2>
-      <p>Paste the text you want questions and answers to be generated from. Ex. Apples are colored red.</p>
-      <input onChange = {change} placeholder="Enter text here"
-      value = {prompt}/>
-      <button onClick = {chatgptrequest}>Generate Flash cards </button>
-    </div>
-  )
-}
+	return (
+		<div className="Component">
+			<h2>Make Flash Cards!</h2>
+			<p>
+				Paste the text you want questions and answers to be generated from. Ex.
+				Apples are colored red.
+			</p>
+			<input
+				onChange={handleChange}
+				placeholder="Enter text here"
+				value={userInput}
+			/>
+			<button onClick={chatgptrequest}>Generate Flash cards </button>
+			<p>{jsonOutput}</p>
+		</div>
+	);
+};
 
 export default CreateCards;
-  
