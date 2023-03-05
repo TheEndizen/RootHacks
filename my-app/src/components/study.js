dcb03ef331eddaa96183859dae60073bc7d17004
Amy Cao <caoamyc@sfu.ca>
2023-03-04 18:19:21 -0800

    add images into flashcards

--- my-app/src/components/study.js
+++ my-app/src/components/study.js
@@ -2,14 +2,15 @@
 import React from 'react';
 import ReactCardFlip from 'react-card-flip';
 import { useState } from "react";
+import "../images/turnip_1.jpg";
+import "../images/turnip_2.jpg";
+import "../images/turnip_3.jpg";
+import "../images/turnip_4.jpg";
+import "../images/turnip_5.jpg";
 
-function FrontCard() {
-  // get question from db
-  return (<div>front card
 
-     </div>
-     )
-}
+
+// Math.floor(Math.random() * (5 - 1 + 1)) + 1
 
 function BackCard(){
   // get answer from db
@@ -17,10 +18,44 @@ function BackCard(){
 }
 
 
-
-
 function FlipCard() {
+
   const [flip, setFlip] = useState(false);
+  const [correctCnt, setCorrectCnt] = useState(0);
+  const [incorrectCnt, setIncorrectCnt] = useState(0);
+
+const handleCorrect = () => {
+  console.log("correct count:", correctCnt)
+  setCorrectCnt(correctCnt + 1);
+  setFlip(!flip)
+};
+
+const handleIncorrect = () => {
+  setIncorrectCnt(incorrectCnt + 1);
+  setFlip(!flip)
+};
+
+const FrontCard = () => {
+  if (correctCnt != 1 && correctCnt%2 == 0){
+    console.log("showing turnpip")
+    let path = "../images/turnip_"+ Math.floor(Math.random() * (5 - 1 + 1)) + 1 
+    return (
+      <div>
+      front card
+       <img src={path} className="App-logo" alt="logo" />
+       </div>
+    )
+
+  }
+  else {
+    return (<div>front card
+    
+      </div>
+      )
+    }
+}
+
+
 return (
    <div className="Card">
     <ReactCardFlip isFlipped={flip} 
@@ -69,8 +104,18 @@ return (
                 background: '#f5d9fa',
                 fontWeight: 'bold',
                 borderRadius: '5px'
-            }} onClick={() => setFlip(!flip)}>
-                Flip</button>
+            }} onClick={() => handleIncorrect() }>
+                Incorrect</button>
+
+                <button style={{
+                width: '150px',
+                padding: '10px',
+                fontSize: '20px',
+                background: '#f5d9fa',
+                fontWeight: 'bold',
+                borderRadius: '5px'
+            }} onClick={() => handleCorrect()}>
+                Correct</button>
         </div>
     </ReactCardFlip>
     </div>
