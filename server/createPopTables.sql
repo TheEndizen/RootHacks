2098e1ea0871eb2eab85cab185d7a10e09125fb9
Denise Siu <68887691+djsiu@users.noreply.github.com>
2023-03-04 17:27:56 -0800

    adding data tbl

--- server/createPopTables.sql
+++ server/createPopTables.sql
@@ -2,6 +2,17 @@ CREATE TABLE Test (
     id char(50) PRIMARY KEY
 );
 
+CREATE TABLE ChatGPTAnswers (
+    id int PRIMARY KEY,
+    ans char(4000)
+);
+
+CREATE TABLE Cards (
+    id int PRIMARY KEY,
+    ques char(1000),
+    ans char(1000)
+)
+
 INSERT INTO test (id)
     VALUES ('someone 1');
 INSERT INTO test (id)
